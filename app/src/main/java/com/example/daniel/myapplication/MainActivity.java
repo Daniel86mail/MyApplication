a970c06cf127cb5734b3bfb46e34425a82cb5ad7
Daniel86mail <daniel86mail@gmail.com>
2015-01-14 15:51:32 +0200

    MyFirstAndroidApp

---
+++ app/src/main/java/com/example/daniel/myapplication/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.daniel.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.daniel.myapplication.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+    public void SendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+    public void openSearch(){
+
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        String message = "test test test";
+        //intent.putExtra(EXTRA_MESSAGE, message);
+        //startActivity(intent);
+        TextView textView = new TextView(getApplicationContext());
+        textView.setText(message);
+        setContentView(textView);
+    }
+
+    public void openSettings() {
+
+    }
+
+
+
+}
